caa33bbb7695cbfab1bf56835f4810e02bfa9be5
zolal-ayazpour <zolalaya@buffalo.edu>
2019-09-17 17:48:10 -0400

    Modified!

--- week_01/case_study/MeanANDHist.R
+++ week_01/case_study/MeanANDHist.R
@@ -1,7 +1,15 @@
 library(dplyr)
+
+#Read iris data
 data(iris)
+
+#Petal length data
 petal_length <- iris$Petal.Length
+
+#Mean
 mean_petal_length <- mean(petal_length)
+
+#Histogram
 histogram_petal_length <- hist(petal_length,
                                main="Histogram of Petal Length",
                                xlab="Petal Length",
@@ -9,5 +17,7 @@ histogram_petal_length <- hist(petal_length,
                                ylim=c(0,40),
                                las=1,
                                breaks=11)
+
+#Statistical calculations
 iris$Petal.Length %>%
   summary()
