cddef8223f4113e5f9bf32efe52d235f45bad354
zolal-ayazpour <zolalaya@buffalo.edu>
2019-09-19 17:14:27 -0400

    CaseStudy-2

--- week_02/case_study/Temperature.R
+++ week_02/case_study/Temperature.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-# define the link to the data - you can try this in your browser too
+#define the link to the data - you can try this in your browser too
 dataurl="https://raw.githubusercontent.com/AdamWilsonLab/SpatialDataScience/master/docs/02_assets/buffaloweather.csv"
 
-# Read the data
+#read the data
 temp=read_csv(dataurl,
               skip=1, #skip the first line which has column names
               na="999.90", # tell R that 999.90 means missing in this dataset
@@ -12,10 +12,10 @@ temp=read_csv(dataurl,
                             "AUG","SEP","OCT","NOV",  
                             "DEC","DJF","MAM","JJA",  
                             "SON","metANN"))
-# renaming is necessary becuase they used dashes ("-")
-# in the column names and R doesn't like that.
+#renaming is necessary becuase they used dashes ("-")
+#in the column names and R doesn't like that.
 
-# Plotting the data
+#plotting the data
 title = "Mean Summer Temperature in Buffalo, NY (1880-2018)"
 subtitle= "Summer includes Jun, July, and August \nData from the Global Historical Climate Network \nRed line is a LOESS smooth"
 
